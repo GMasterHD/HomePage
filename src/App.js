aab76d1e4e8a4b67044c3df1aed73f819adfe105
Justin Jakob <xxgmasterhdxx@gmail.com>
2022-05-06 18:13:33 +0200

    Added default sructure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+/* ---> Website Elements <--- */
+import Header from './elements/Header'
+import Footer from './elements/Header'
+
+/* ---> Pages <--- */
+import Main from './pages/Main'
+import NotFound from './pages/NotFound'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Header />
+
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Main />}/>
+					<Route path='*' element={<NotFound />}/>
+				</Routes>
+			</BrowserRouter>
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function NotFound(props) {
+  return (
+	<div>404 - Not Found</div>
+  )
+}
+
+NotFound.propTypes = {}
+
+export default NotFound
+
